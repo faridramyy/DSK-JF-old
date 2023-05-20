0e18054c0caca54ca228566fe7622de8138c3705
farid <faridramy2003@gmail.com>
2023-05-21 00:40:03 +0300

    added the routes for the home pages

--- app.js
+++ app.js
@@ -4,19 +4,15 @@ import mongoose from "mongoose";
 import session from "express-session";
 import resgisterRouter from "./routes/registerRouter.js";
 import adminRouter from "./routes/adminRouter.js";
+import homeRouter from "./routes/homeRouter.js";
 import { isAuthenticated } from "./middlewares/userAuth.js";
 import { adminIsAuthenticated } from "./middlewares/adminAuth.js";
 import { Configuration, OpenAIApi } from "openai";
-
-//dalia
-const configuration = new Configuration({
-  apiKey: "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",
-});
-
 dotenv.config();
 
 const app = express();
 const port = process.env.port;
+
 //middlewares
 app.use(express.static("public"));
 app.use(express.json()); // parse request body to JSON
@@ -31,8 +27,43 @@ app.use(
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(homeRouter);
+app.use(resgisterRouter);
+app.use("/admin", adminIsAuthenticated, adminRouter);
+app.get("/logout", function (req, res, next) {
+  req.session.user = null;
+  req.session.save(function (err) {
+    if (err) next(err);
+    req.session.regenerate(function (err) {
+      if (err) next(err);
+      res.redirect("/login");
+    });
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).render("404");
+});
+
+mongoose
+  .connect(process.env.mongooDbUrl)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  })
+  .catch(() => {
+    console.log("Couldn't connect to Database");
+  });
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+app.get("/home", isAuthenticated, function (req, res) {
+  res.send("home");
+});
+
+//dalia
+const configuration = new Configuration({
+  apiKey: "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",
 });
 
 //dalia
@@ -50,49 +81,3 @@ app.post("/chat", async (req, res) => {
 app.get("/chat", (req, res) => {
   res.render("chat.ejs", { message: req.query.message });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.use(resgisterRouter);
-app.use("/admin", adminIsAuthenticated, adminRouter);
-
-app.get("/home", isAuthenticated, function (req, res) {
-  res.send("home");
-});
-
-app.get("/logout", function (req, res, next) {
-  req.session.user = null;
-  req.session.save(function (err) {
-    if (err) next(err);
-    req.session.regenerate(function (err) {
-      if (err) next(err);
-      res.redirect("/login");
-    });
-  });
-});
-
-mongoose
-  .connect(process.env.mongooDbUrl)
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
-    });
-  })
-  .catch(() => {
-    console.log("Couldn't connect to Database");
-  });
