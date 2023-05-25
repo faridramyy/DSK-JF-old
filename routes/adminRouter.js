879ac2fe36d50b7dd8121d545773ee37bb34fc98
Dalia Raafat <daliaraafat03@gmail.com>
2023-05-25 17:13:04 +0200

    Security router

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -4,5 +4,8 @@ const router = express.Router();
 router.get("/editProfile", (req, res) => {
   res.render("admin/editProfile");
 });
+router.get("/security", (req, res) => {
+  res.render("admin/security");
+});
 
 export default router;
