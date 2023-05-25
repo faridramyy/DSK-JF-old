1e82acfcc85d7c2290489fab04e6390405703cce
Dalia Raafat <daliaraafat03@gmail.com>
2023-05-25 21:33:10 +0200

    .

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -14,17 +14,17 @@ router.get("/settings", (req, res) => {
   res.render("admin/settings");
 });
 
-// router.get("/", (req, res) => {
-//   res.render("admin/dashboard");
-// });
-// router.get("/users", (req, res) => {
-//   res.render("admin/users");
-// });
-// router.get("/courses", (req, res) => {
-//   res.render("admin/courses");
-// });
-// router.get("/settings", (req, res) => {
-//   res.render("admin/settings");
-// });
+router.get("/", (req, res) => {
+  res.render("admin/dashboard");
+});
+router.get("/users", (req, res) => {
+  res.render("admin/users");
+});
+router.get("/courses", (req, res) => {
+  res.render("admin/courses");
+});
+router.get("/settings", (req, res) => {
+  res.render("admin/settings");
+});
 
 export default router;
