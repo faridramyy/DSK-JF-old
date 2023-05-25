160b1ef3c00b28a80fc8ddf88cd579509bc817cd
Dalia Raafat <daliaraafat03@gmail.com>
2023-05-25 21:32:26 +0200

    ..

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,31 +1,30 @@
 import express from "express";
 const router = express.Router();
 
-router.get("/",(req,res)=>{
-    res.render("admin/dashboard")
-})
-router.get("/users",(req,res)=>{
-    res.render("admin/users")
-})
-router.get("/courses",(req,res)=>{
-    res.render("admin/courses")
-})
-router.get("/settings",(req,res)=>{
-    res.render("admin/settings")
-})
-
 router.get("/", (req, res) => {
-  res.render("admin/dashboard")
-})
-router.get("/users",(req,res)=>{
-  res.render("admin/users")
-})
-router.get("/courses",(req,res)=>{
-  res.render("admin/courses")
-})
+  res.render("admin/dashboard");
+});
+router.get("/users", (req, res) => {
+  res.render("admin/users");
+});
+router.get("/courses", (req, res) => {
+  res.render("admin/courses");
+});
 router.get("/settings", (req, res) => {
-  res.render("admin/settings")
+  res.render("admin/settings");
 });
 
-export default router
+// router.get("/", (req, res) => {
+//   res.render("admin/dashboard");
+// });
+// router.get("/users", (req, res) => {
+//   res.render("admin/users");
+// });
+// router.get("/courses", (req, res) => {
+//   res.render("admin/courses");
+// });
+// router.get("/settings", (req, res) => {
+//   res.render("admin/settings");
+// });
 
+export default router;
