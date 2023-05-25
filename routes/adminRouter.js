128ca34addd9d48dfa5dcb8a93a8f1d630c6d584
farid <faridramy2003@gmail.com>
2023-05-25 21:27:22 +0300

    admin settings 1

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,31 +1,23 @@
 import express from "express";
 const router = express.Router();
 
-router.get("/",(req,res)=>{
-    res.render("admin/dashboard")
-})
-router.get("/users",(req,res)=>{
-    res.render("admin/users")
-})
-router.get("/courses",(req,res)=>{
-    res.render("admin/courses")
-})
-router.get("/settings",(req,res)=>{
-    res.render("admin/settings")
-})
-
 router.get("/", (req, res) => {
-  res.render("admin/dashboard")
-})
-router.get("/users",(req,res)=>{
-  res.render("admin/users")
-})
-router.get("/courses",(req,res)=>{
-  res.render("admin/courses")
-})
+  res.render("admin/dashboard");
+});
+router.get("/users", (req, res) => {
+  res.render("admin/users");
+});
+router.get("/courses", (req, res) => {
+  res.render("admin/courses");
+});
 router.get("/settings", (req, res) => {
-  res.render("admin/settings")
+  res.render("admin/settings");
+});
+router.get("/security", (req, res) => {
+  res.render("admin/security");
+});
+router.get("/notifications", (req, res) => {
+  res.render("admin/notifications");
 });
 
-export default router
-
+export default router;
