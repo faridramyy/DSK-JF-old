ddf9777440bdb02148b017bdca18ec1094ad24fe
farid <faridramy2003@gmail.com>
2023-05-26 04:45:21 +0300

    fix code

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,11 +1,17 @@
 import express from "express";
+import userModel from "../models/user.js";
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.render("admin/dashboard");
 });
-router.get("/users", (req, res) => {
-  res.render("admin/users");
+router.get("/users", async (req, res) => {
+  const page = req.query.p || 0;
+  const usersPerPage = 3;
+  let users = await userModel.find()
+    .skip(page * usersPerPage)
+    .limit(usersPerPage);
+  res.render("admin/users", { users });
 });
 router.get("/courses", (req, res) => {
   res.render("admin/courses");
