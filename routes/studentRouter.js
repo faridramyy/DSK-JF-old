de7088539ea45ddfa49449a7f3885c7e8fb18e07
Dalia Raafat <daliaraafat03@gmail.com>
2023-05-28 15:58:04 +0200

    Added courses router

--- routes/studentRouter.js
+++ routes/studentRouter.js
@@ -1,18 +1,27 @@
 import express from "express";
 import UserModel from "../models/user.js";
-
+import courseModel from "../models/course.js";
 const router = express.Router();
 
 const findUser = async (id) => {
   return await UserModel.findById(id);
 };
 
-router.get("/gotbanned", (req, res) => {
+router.get("student/gotbanned", (req, res) => {
   res.render("student/gotBanned", { user: findUser(req.params.id) });
 });
 
+router.get("/courses", async (req, res) => {
+  // const page = req.query.p || 0;
+  // const usersPerPage = 3;
+  let courses = await courseModel.find();
+  // .skip(page * usersPerPage)
+  // .limit(usersPerPage);
+  res.render("student/courses", { courses });
+});
+
 router.get("/:id", (req, res) => {
-  res.render("student/home", { user: findUser(req.params.id) });
+  res.render("student/home");
 });
 
 export default router;
