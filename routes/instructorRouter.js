0c13ba7c05ad32c70439dc62ccaffd2df5dd3cd6
karenossama <123019055+karenossama@users.noreply.github.com>
2023-05-31 18:58:06 +0300

    added the admin router

---
+++ routes/instructorRouter.js
@@ -0,0 +1,23 @@
+import express from "express";
+import courseModel from "../models/course.js";
+const router = express.Router();
+
+
+
+router.get("/:id", async (req, res) => {
+  try {
+    const instructorId = req.params.id;
+    let courses = await courseModel.find({ instructorId: instructorId });
+    res.render("instructor/home", { courses });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.get("/:id/course", (req, res) => {
+  res.render("instructor/course");
+});
+
+
+export default router;
