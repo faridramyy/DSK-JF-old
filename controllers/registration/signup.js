96ca80a2fab0bffac1eaf2b93414b56a4a0c10a3
farid <faridramy2003@gmail.com>
2023-06-04 18:27:42 +0300

    finished signup page

---
+++ controllers/registration/signup.js
@@ -0,0 +1,59 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import userModel from "../../models/user.js";
+
+const signup_get = (req, res) => res.render("registration/signup");
+
+const signup_post = async (req, res) => {
+  const {
+    role,
+    firstName,
+    lastName,
+    username,
+    email,
+    password,
+    gpa,
+    createdByAdmin = 0,
+  } = req.body;
+
+  try {
+    if (await userModel.findOne({ email }))
+      return res.status(409).json({ errMsg: "Email is Taken" });
+
+    if (await userModel.findOne({ username }))
+      return res.status(409).json({ errMsg: "Usernmae is Taken" });
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      role,
+      firstName,
+      lastName,
+      username,
+      email,
+      password: hashedPassword,
+      gpa,
+    });
+
+    newUser.save();
+
+    if (!createdByAdmin) {
+      const token = jwt.sign({ user: newUser }, process.env.jwtSecretPhrase, {
+        expiresIn: 3 * 24 * 60 * 60, //3 days
+      });
+
+      res.cookie("jwt", token, {
+        httpOnly: true,
+        maxAge: 3 * 24 * 60 * 60 * 1000, //3 days
+      });
+    }
+
+    res.status(201).json({ user: newUser }); //code 201 for created success
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err: true });
+  }
+};
+
+export default { signup_get, signup_post };
--- routes/registration.js
+++ routes/registration.js
@@ -1,12 +1,12 @@
 import express from "express";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 import nodemailer from "nodemailer";
 
 import userModel from "../models/user.js";
 import otpModel from "../models/otp.js";
 
 import login from "../controllers/registration/login.js";
+import signup from "../controllers/registration/signup.js";
 
 const router = express.Router();
 
@@ -14,59 +14,8 @@ router.get("/login", login.login_get);
 router.post("/login", login.login_post);
 
 
-router.get("/signup", (req, res) => res.render("registration/signup"));
-
-router.post("/signup", async (req, res) => {
-  const {
-    role,
-    firstName,
-    lastName,
-    username,
-    email,
-    password,
-    gpa,
-    createdByAdmin = 0,
-  } = req.body;
-
-  try {
-    if (await userModel.findOne({ email }))
-      return res.status(409).json({ errMsg: "Email is Taken" });
-
-    if (await userModel.findOne({ username }))
-      return res.status(409).json({ errMsg: "Usernmae is Taken" });
-
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    const newUser = new userModel({
-      role,
-      firstName,
-      lastName,
-      username,
-      email,
-      password: hashedPassword,
-      gpa,
-    });
-
-    newUser.save();
-
-    if (!createdByAdmin) {
-      const token = jwt.sign({ user: newUser }, process.env.jwtSecretPhrase, {
-        expiresIn: 3 * 24 * 60 * 60, //3 days
-      });
-
-      res.cookie("jwt", token, {
-        httpOnly: true,
-        maxAge: 3 * 24 * 60 * 60 * 1000, //3 days
-      });
-    }
-
-    res.status(201).json({ user: newUser }); //code 201 for created success
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ err: true });
-  }
-});
+router.get("/signup",signup.signup_get );
+router.post("/signup", signup.signup_post);
 
 router.get("/forgetpassword", (req, res) =>
   res.render("registration/forgetpassword")
